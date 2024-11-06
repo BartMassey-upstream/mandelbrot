ad6d4446f0672a5d06bb7c9b082943a2196244e5
Bart Massey <bart.massey@gmail.com>
2024-11-06 12:53:29 -0800

    upgraded to 2021 Edition, updated to latest dependencies

--- src/main.rs
+++ src/main.rs
@@ -130,21 +130,25 @@ fn render(pixels: &mut [u8],
     }
 }
 
-use image::ColorType;
-use image::png::PNGEncoder;
+use image::{
+    codecs::png::PngEncoder,
+    error::ImageResult,
+    ExtendedColorType,
+    ImageEncoder,
+};
 use std::fs::File;
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
 /// file named `filename`.
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
-    -> Result<(), std::io::Error>
+    -> ImageResult<()>
 {
     let output = File::create(filename)?;
 
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels,
+    let encoder = PngEncoder::new(output);
+    encoder.write_image(&pixels,
                    bounds.0 as u32, bounds.1 as u32,
-                   ColorType::Gray(8))?;
+                   ExtendedColorType::L8)?;
 
     Ok(())
 }
